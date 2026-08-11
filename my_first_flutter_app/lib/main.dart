d009892b415845762e59ea675a6a4249d4748b8a
domsprog <domsocarol@gmail.com>
2026-08-11 21:33:17 +0800

    FEAT: Added flutter Activity 2

--- my_first_flutter_app/lib/main.dart
+++ my_first_flutter_app/lib/main.dart
@@ -15,7 +15,7 @@ class MyApp extends StatelessWidget {
         colorScheme: ColorScheme.fromSeed(
           seedColor: const Color.fromARGB(255, 213, 230, 214),
         ),
-        scaffoldBackgroundColor: const Color.fromARGB(255, 3, 3, 3), 
+        scaffoldBackgroundColor: const Color.fromARGB(255, 3, 3, 3),
       ),
       home: const MyHomePage(title: 'My First Flutter Application'),
     );
@@ -36,35 +36,136 @@ class _MyHomePageState extends State<MyHomePage> {
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
-        title: Text(widget.title,
-         style: const TextStyle(color: Colors.white, 
-         fontWeight: FontWeight.bold)
+        title: Text(
+          widget.title,
+          style: const TextStyle(
+            color: Colors.white,
+            fontWeight: FontWeight.bold,
+          ),
+          textAlign: TextAlign.center,
         ),
         backgroundColor: const Color.fromARGB(255, 3, 3, 3),
       ),
-      body: Padding(
-        padding: const EdgeInsets.all(24.0),
-        child: Column(
-          mainAxisAlignment: MainAxisAlignment.start,
-          children: [
-            const SizedBox(height: 20),
-            const Text(
-              'Dominic Ocarol, BSIT-3, 07-30,2026',
-              style: TextStyle(
-                fontSize: 24,
-                fontWeight: FontWeight.bold,
-                color: Colors.white,
+
+      body: Column(
+        mainAxisAlignment: MainAxisAlignment.start,
+        children: [
+          //Application Title
+          const Text(
+            'Welcome to My First Flutter Application\n',
+            style: TextStyle(
+              fontSize: 24,
+              color: Colors.white,
+              fontStyle: FontStyle.italic,
+            ),
+            textAlign: TextAlign.center,
+          ),
+
+          const SizedBox(height: 20),
+
+          CircleAvatar(
+            radius: 100,
+            backgroundColor: Colors.white,
+            backgroundImage: const AssetImage('assets/images/batman.jpg'),
+          ),
+
+          const SizedBox(height: 20),
+
+          // Name and Course
+          Row(
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: [
+              const Text(
+                'Dominic Ocarol\n',
+                style: TextStyle(
+                  fontSize: 30,
+                  fontWeight: FontWeight.bold,
+                  color: Colors.white,
+                ),
               ),
-              textAlign: TextAlign.center,
+              const SizedBox(width: 20),
+
+              const Text(
+                'BSIT 3 - 1\n',
+                style: TextStyle(fontSize: 20, color: Colors.white),
+              ),
+            ],
+          ),
+
+          const SizedBox(height: 10),
+
+          // Hobby
+          const Text(
+            'Hobby: Playing Video Games, Watching Movies, and Reading Books\n',
+            style: TextStyle(fontSize: 24, color: Colors.white),
+            textAlign: TextAlign.center,
+          ),
+
+          const SizedBox(height: 5),
+
+          // Date
+          const Text(
+            'June 6, 2024',
+            style: TextStyle(fontSize: 15, color: Colors.white),
+
+            textAlign: TextAlign.center,
+          ),
+
+          const SizedBox(height: 20),
+
+          // Favorite Game
+          Row(
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: [
+              Column(
+                children: const [
+                  Text(
+                    'My favorite game is:  Dota2 , Valorant',
+                    style: TextStyle(
+                      fontSize: 20,
+                      fontWeight: FontWeight.bold,
+                      color: Colors.white,
+                    ),
+                  ),
+                ],
+              ),
+            ],
+          ),
+
+          const SizedBox(height: 20),
+
+          // Favorite Movie and Food
+          Card(
+            child: Padding(
+              padding: const EdgeInsets.all(8.0),
+              child: const Text(
+                'My favorite movie is: The Dark Knight\n',
+                style: TextStyle(
+                  color: Colors.black,
+                  fontSize: 12,
+                  fontWeight: FontWeight.bold,
+                ),
+              ),
             ),
-            const SizedBox(height: 50),
-            CircleAvatar(
-              radius: 100,
-              backgroundColor: Colors.white,
-              backgroundImage: const AssetImage('assets/images/batman.jpg'),    
+          ),
+
+          // Favorite Food
+          Card(
+            child: Padding(
+              padding: const EdgeInsets.all(8.0),
+              child: const Text(
+                'My favorite Food is: Adobo\n',
+                style: TextStyle(
+                  fontSize: 12,
+                  fontWeight: FontWeight.bold,
+                  color: Colors.black,
+                ),
+              ),
             ),
-          ],
-        ),
+          ),
+
+          
+        ],
       ),
     );
   }
